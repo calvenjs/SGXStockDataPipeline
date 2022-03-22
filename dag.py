bdb56adb85466e123269fe261b791e745fff3efe
hewtungyuen <hewtungyuen@gmail.com>
2022-03-22 17:21:05 +0800

    Add dag & connection to bigquery

---
+++ dag.py
@@ -0,0 +1,42 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from financialnews_extraction import extract
+from financialnews_extraction import transform
+from financialnews_extraction import load
+
+with DAG(
+    'financialnews_extraction',
+    default_args = {
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'start_date': datetime(2022, 3, 11),
+        'email': [''],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 0,
+        'retry_delay': timedelta(minutes=1)
+    },
+    schedule_interval=timedelta(days=1),
+    start_date= datetime(2021, 3, 21),
+) as dag:
+
+    financialNewsExtraction = PythonOperator(
+        task_id='financialNewsExtraction',
+        python_callable=extract,
+        dag=dag,
+    )
+
+    financialNewsTransformation = PythonOperator(
+        task_id='financialNewsTransformation',
+        python_callable=transform,
+        dag=dag,
+    )
+
+    financialNewsLoading = PythonOperator(
+        task_id='financialNewsLoading',
+        python_callable=load,
+        dag=dag,  
+    )
+
+    financialNewsExtraction >> financialNewsTransformation >> financialNewsLoading
--- financialnews_extraction.py
+++ financialnews_extraction.py
@@ -1,21 +1,23 @@
 import bs4
 import requests
 import pandas as pd
+from datetime import datetime, timedelta
+import json
+
+from google.cloud import bigquery
+import os
+
 import nltk
-
-URL = "https://www.marketwatch.com/investing/index/sti?countrycode=sg"
-
-headlines = []
-dates = []
-links = []
-sentiment = []
-
-nltk.download('vader_lexicon')
+# nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sid = SentimentIntensityAnalyzer()
 
-
-def extract(url):
+def getBacklog():
+    url = "https://www.marketwatch.com/investing/index/sti?countrycode=sg"
+    headlines = []
+    dates = []
+    links = []
+    sentiment = []
 
     result = requests.get(url, "html.parser")
     soup = bs4.BeautifulSoup(result.text, "html.parser")
@@ -24,38 +26,144 @@ def extract(url):
     for row in text:
         try:
             headline = row.h3.text.strip()
-            headlines.append(headline)
-            
             sentiment_score = sid.polarity_scores(headline)['compound']
-            sentiment.append(sentiment_score)
-            
             
         except:
             print("headline missing")
-            headlines.append(None)
-            sentiment.append(None)
-        
+            continue
+            
         try:
             date = row.find(class_ = "article__timestamp")["data-est"]
-            dates.append(date)
         except:
             print("date missing")
-            dates.append(None)
+            continue
             
         try:
             link = row.find(class_ = "link")["href"]
-            links.append(link)
         except:
             print("link missing")
-            links.append(None)
+            continue
 
+        headlines.append(headline)
+        sentiment.append(sentiment_score)
+        dates.append(datetime.strptime(date[:10],'%Y-%m-%d'))
+        links.append(link)
 
-extract(URL)
+    df = pd.DataFrame({"Headline": headlines, "Date": dates, "URL": links, "Sentiment": sentiment})
+    # print(df)
 
+    credentials_path = 'C:/Users/hewtu/Documents/GitHub/SGXStockDataPipeline/key.json'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= credentials_path
 
-df = pd.DataFrame({"Headline": headlines, "Dates": dates, "Links": links, "Sentiment score": sentiment})
+    client = bigquery.Client()
+    table_id = "bustling-brand-344211.IS3107Project.News Sentiment"  
 
-for index, row in df.iterrows():
-    print(row['Headline'])
-    print(row['Sentiment score'])
-    print()
+    job_config = bigquery.LoadJobConfig(schema=[
+        bigquery.SchemaField("Headline", "STRING"),
+        bigquery.SchemaField("Date", "TIMESTAMP"),
+        bigquery.SchemaField("URL", "STRING"),
+        bigquery.SchemaField("Sentiment", "FLOAT"),
+    ])
+
+    job = client.load_table_from_dataframe(
+        df, table_id, job_config=job_config
+    )
+
+    job.result()
+
+    table = client.get_table(table_id)  # Make an API request.
+    print(
+        "Loaded {} rows and {} columns to {}".format(
+            table.num_rows, len(table.schema), table_id
+        )
+    )
+    
+getBacklog()
+
+def extract(ti):
+    url = "https://www.marketwatch.com/investing/index/sti?countrycode=sg"
+
+    result = requests.get(url, "html.parser")
+    soup = bs4.BeautifulSoup(result.text, "html.parser")
+    text = soup.find_all(class_= "element element--article")
+
+    headline_data_link = {"Headline": [], "Date": [], "Link": [], "Sentiment": []}
+
+    for row in text:
+        try:
+            date = row.find(class_ = "article__timestamp")["data-est"] 
+            
+        except:
+            print('date mising')
+            continue
+
+        if datetime.strptime(date[:10],'%Y-%m-%d') == datetime(2022,3,19): #datetime.today() - timedelta(1):
+            try:
+                headline_data_link['Date'].append(date)
+                headline = row.h3.text.strip()
+                headline_data_link['Headline'].append(headline)
+
+            except:
+                print('headline missing')
+            
+            try:
+                link = row.find(class_ = "link")["href"]
+                headline_data_link['Link'].append(link)
+            except:
+                print('link missing')
+
+    ti.xcom_push(key = 'headline_date_link', value = headline_data_link)
+
+def transform(ti):
+    headline_date_link = ti.xcom_pull(key='headline_date_link', task_ids = ['financialNewsExtraction'])[0]
+
+    headlines = headline_date_link['Headline']
+
+    if len(headlines) == 0:
+        ti.xcom_push(key = 'no articles', value = True)
+        return 
+    
+    scores = []
+    for headline in headlines:
+        score = sid.polarity_scores(headline)['compound']
+        scores.append(score)
+
+    ti.xcom_push(key = 'scores', value = scores)
+
+def load(ti):
+    if ti.xcom_pull(key = 'no articles', task_ids = ['financialNewsExtraction']):
+        print('No articles for the day')
+        return 
+    headline_date_link = ti.xcom_pull(key='headline_date_link', task_ids = ['financialNewsExtraction'])[0]
+    scores = ti.xcom_pull(key='scores', task_ids = ['financialNewsTransformation'])[0]
+    headline_date_link['Sentiment'].extend(scores)
+    df = pd.DataFrame(headline_date_link)
+
+    # print(df)
+    # print(headline_date_link)
+    
+    credentials_path = 'C:/Users/hewtu/Documents/GitHub/SGXStockDataPipeline/key.json'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= credentials_path
+
+    client = bigquery.Client()
+    table_id = "bustling-brand-344211.IS3107Project.News Sentiment"  
+
+    job_config = bigquery.LoadJobConfig(schema=[
+        bigquery.SchemaField("Headline", "STRING"),
+        bigquery.SchemaField("Date", "TIMESTAMP"),
+        bigquery.SchemaField("URL", "STRING"),
+        bigquery.SchemaField("Sentiment", "FLOAT"),
+    ])
+
+    job = client.load_table_from_dataframe(
+        df, table_id, job_config=job_config
+    )
+
+    job.result()
+
+    table = client.get_table(table_id)  # Make an API request.
+    print(
+        "Loaded {} rows and {} columns to {}".format(
+            table.num_rows, len(table.schema), table_id
+        )
+    )
