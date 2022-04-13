db460bdf61c4376adcb9f40588d5afd8e1252c6e
lrqianqian <70301065+132468756@users.noreply.github.com>
2022-04-13 14:33:28 +0800

    Update financials.py, comments written

--- financials.py
+++ financials.py
@@ -4,8 +4,10 @@ import pandas as pd
 from google.cloud import bigquery
 import os
 
+# Get company_info for all the STI components and store them into dataframes
 def financials_extract(ti):
 
+    # Get tickers for STIcomponets using xcom
     df = ti.xcom_pull(key='STIcomponents', task_ids=['STIExtract'])[0]
     df = pd.DataFrame(eval(df))
 
@@ -23,16 +25,18 @@ def financials_extract(ti):
 
     ti.xcom_push(key = 'company info', value = company_info)
 
+# Extract key information or fields from company_info and convert into json format
 def financials_transform(ti):
     company_info = ti.xcom_pull(key = 'company info', task_ids=['financialsExtract'])
     numOfCompany = len(company_info[0])
 
-    # Need to decide what info to use
+    # Key information to be used
     dic = {'Company_Name': [company_info[0][0]['shortName']], 'Ticker': [company_info[0][0]['symbol']], 'Gross_Profits': [company_info[0][0]['grossProfits']],
            'Total_Debt': [company_info[0][0]['totalDebt']], 'Total_Cashflow': [company_info[0][0]['operatingCashflow']],
            'Total_Revenue': [company_info[0][0]['totalRevenue']], 'Net_Income': [company_info[0][0]['netIncomeToCommon']],
            'Return_On_Equity': [company_info[0][0]['returnOnEquity']], 'Book_per_Share': [company_info[0][0]['bookValue']]}
     
+    # Convert into dataframe
     df = pd.DataFrame(dic)
     
     for i in range(1, numOfCompany):
@@ -50,12 +54,13 @@ def financials_transform(ti):
     df = df.to_json(orient='records')
     ti.xcom_push(key="processed_user_info", value=df)
 
+# Create financials table in the staging area if not exist, and push the info for each company in the json into the table
 def financials_staging(ti):
     results = ti.xcom_pull(task_ids=["financialsTransform"], key="processed_user_info")
     df = pd.DataFrame(eval(results[0]))
     df['Date'] = df['Date'].apply(lambda x: datetime.datetime.fromtimestamp(int(x)/1000))
 
-    # Change the bigquery path and table name
+    # Connect to staging area, create staging table if not exist
     credentials_path = 'key.json'
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
     client = bigquery.Client()
@@ -104,7 +109,7 @@ def financials_staging(ti):
 
     print('Successfully loaded company financials')
 
-
+# Load information in the staging table into the storage table
 def financials_load():
     credentials_path = 'key.json'
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
