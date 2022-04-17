7b399c78634483713c79570b721bed757675ca0b
Tay Xun Yang <gratzxy@gmail.com>
2022-04-17 10:35:05 +0800

    Documentation

--- STI_components.py
+++ STI_components.py
@@ -2,24 +2,28 @@ import pandas as pd
 import requests
 
 def STIextraction(ti):
-
+    #Retrieve STI Index (^STI) components
     try:
+  
         url = 'https://sginvestors.io/analysts/sti-straits-times-index-constituents-target-price'
         r = requests.get(url, headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
         payload=pd.read_html(r.text)
         df = payload[0]
         df = df[['Straits Times Index STI Constituent']]
         df = df.dropna()
-
+        
         tickers = []
         companyNames = []
+        #Store STI Components in List
         for index, row in df.iterrows():
             tickers.append(row['Straits Times Index STI Constituent'].split('(')[1].split(')')[0][4:] + '.SI')
             companyNames.append(row['Straits Times Index STI Constituent'].split('(')[0])
             
+        #Convert List to Dataframe
         df = pd.DataFrame({'Ticker':tickers, 'Company name': companyNames})
         df.to_csv("STI_components.csv", index=False)
         df = df.to_json(orient='records')
+        #Push STI components to pipeline
         ti.xcom_push(key='STIcomponents', value=df)
     except:
         df = pd.read_csv("STI_components.csv")
